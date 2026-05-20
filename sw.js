fa8eb536c88461000eb1249542ad364ae7372c2d
rene estripeaut <reneestripeaut@renes-MacBook-Pro.local>
2026-05-20 07:25:39 -0500

    feat: display mascot_question on Yesterday page

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // DOPAmine Service Worker
 // Version 1.0 — Push Notifications
 
-const CACHE_NAME = 'dopamine-v37';
+const CACHE_NAME = 'dopamine-v38';
 
 // Install — activate immediately
 self.addEventListener('install', event => {
